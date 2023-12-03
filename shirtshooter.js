76f425e5b9e7f74b64adae4442bedfe33c817c6a
gotemilk <44422917+gotemilk@users.noreply.github.com>
2023-12-02 23:36:14 -0700

    Update shirtshooter.js

--- shirtshooter.js
+++ shirtshooter.js
@@ -1,6 +1,6 @@
  var i = 0; // Start point
   var images = [];
-  var time = 1500;
+  var time = 2000;
   
   // image list
      
@@ -11,36 +11,11 @@
   images[4] = 'https://res.cloudinary.com/dc0eqkfuj/image/upload/v1701572306/NICO_DALMATIAN_dgxdzg.jpg';
       
       
+    images[5] = 'https://res.cloudinary.com/dc0eqkfuj/image/upload/v1701578425/FLY_HONEY_MAN2_f1b6ac.jpg'
       
-       images[5] = 'https://res.cloudinary.com/dc0eqkfuj/image/upload/v1701572306/MARIA_AND_ARTPAPI_jai5sc.jpg';
+     
   images[6] = 'https://res.cloudinary.com/dc0eqkfuj/image/upload/v1701572306/SAVINGS_DAWSON_xluzok.jpg';
       
-      
-       images[7] = 'https://res.cloudinary.com/dc0eqkfuj/image/upload/v1701572305/JON_STORTS_WITH_FRIENDS_zxbi7d.jpg';
-      
-       images[8] = 'https://res.cloudinary.com/dc0eqkfuj/image/upload/v1701572305/SOME_KIND_OF_GO_KART_OR_SOMETHING_rcsqa3.jpg';
-      
-      
-       images[9] = 'https://res.cloudinary.com/dc0eqkfuj/image/upload/v1701572305/SISTER_KHALISOL_wry42r.jpg';
-      
-       images[10] = 'https://res.cloudinary.com/dc0eqkfuj/image/upload/v1701572306/LAUNCHPAD_MECHERO_3_pie9yw.jpg';
-      
-       images[11] = 'https://res.cloudinary.com/dc0eqkfuj/image/upload/v1701572304/BOYS_PLAYING_WITH_A_PUPPY_c7mecr.jpg';
-      
-    images[12] = 'https://res.cloudinary.com/dc0eqkfuj/image/upload/v1701578425/FLY_HONEY_MAN2_f1b6ac.jpg'
-     
-       images[13] = 'https://res.cloudinary.com/dc0eqkfuj/image/upload/v1701572305/FLIPPED_BENZ_zpefur.jpg';
-      
-      
-       images[14] = 'https://res.cloudinary.com/dc0eqkfuj/image/upload/v1701572305/JAURA_SMOKING_mjhyqw.jpg';
-      
-      images[15] = 'https://res.cloudinary.com/dc0eqkfuj/image/upload/v1701578425/GALLUP_METALHEADS5_tcbpxo.jpg';
-      
-      images[16] = 'https://res.cloudinary.com/dc0eqkfuj/image/upload/v1701572305/NICKS_PLACITA_kxynx8.jpg';
-      
-      
-        images[17] = 'https://res.cloudinary.com/dc0eqkfuj/image/upload/v1701572305/KOHL_FLIPPING_ME_OFF_rzqowe.jpg';
-  
   // change image
   function changeImg(){
     document.slide.src = images[i];
